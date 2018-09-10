d7f2712888f167b71fbb73a34107b7d6d2167ced
Nhinh Dao <nhinhdao.vn@gmail.com>
2018-09-10 14:58:42 -0400

    changes in models

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -1,4 +1,4 @@
-#check again if its better to have Friend class or just use :class_name :user
+# check again if its better to have Friend class or just use :class_name :user
 class Friend < ActiveRecord::Base
    has_many :messages
 end
